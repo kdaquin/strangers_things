ab4ffe18711ac3e6f324115146d505c34cd08ddb
Kyle <kyle@daquin.com>
2021-02-06 08:24:37 -0600

    login button

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,11 +4,16 @@ import {
     BrowserRouter as Router,
     Redirect
   } from "react-router-dom";
+ let signOut = () => {
+    // localStorage.removeItem('myToken')
+    console.log('signedout')
+    document.location.href="/";
+  }
 const Title = () => {
     return (
         <div className='title'>
             <div className="username-sign-out">
-            <button className="sign-out">
+            <button className="sign-out" onClick={signOut}>
                 Sign Out
             </button>
             </div>
